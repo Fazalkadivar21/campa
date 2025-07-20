c793ae357982ec25e523d2d978e4081d9d3fd061
fazalkadivar21 <fazalkadivar21@gmail.com>
2025-07-20 13:14:38 +0530

    ADD: story outline

---
+++ src/components/NowServing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NowServing = () => {
+  return (
+    <div className='text-[23vw] lg:text-[25vh] h-screen flex items-center justify-center'>Now Serving</div>
+  )
+}
+
+export default NowServing
