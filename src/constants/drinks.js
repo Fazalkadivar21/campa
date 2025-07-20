036d4cef6a57cc6984f369c5bc3fee34333b9e64
fazalkadivar21 <fazalkadivar21@gmail.com>
2025-07-20 13:13:47 +0530

    ADD: constants

---
+++ src/constants/drinks.js
@@ -0,0 +1,40 @@
+const drinks = [
+  {
+    image: "/images/cola.png",
+    name: "cola",
+    taste: "Cola",
+    color: "#4B2C5E",
+  },
+  {
+    image: "/images/jeera.png",
+    name: "jeera",
+    taste: "Jeera",
+    color: "#7BAE7F",
+  },
+  {
+    image: "/images/orange.png",
+    name: "orange",
+    taste: "Orange",
+    color: "#C75B12",
+  },
+  {
+    image: "/images/cricket.png",
+    name: "cricket",
+    taste: "Lemony",
+    color: "#66A85E",
+  },
+  {
+    image: "/images/lemon.png",
+    name: "lemon",
+    taste: "Lemon",
+    color: "#D6B647",
+  },
+  {
+    image: "/images/powerup.png",
+    name: "powerup",
+    taste: "Strong Cola",
+    color: "#2E4A7D",
+  },
+];
+
+export default drinks;
