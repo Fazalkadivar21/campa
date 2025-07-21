ae27bad28c396cd64ec6bb1f3a0a5193b9546de9
fazalkadivar21 <fazalkadivar21@gmail.com>
2025-07-21 19:39:06 +0530

    UPDATE: bottles

--- src/constants/drinks.js
+++ src/constants/drinks.js
@@ -2,6 +2,7 @@ const drinks = [
   {
     image: "/images/cola.png",
     can: "/images/colaCan.png",
+    bottle: "/images/colaBottle.png",
     name: "cola",
     taste: "Cola",
     color: "#4B2C5E",
@@ -9,6 +10,7 @@ const drinks = [
   {
     image: "/images/jeera.png",
     can: "/images/jeeraCan.png",
+    bottle: "/images/jeeraBottle.png",
     name: "jeera",
     taste: "Jeera",
     color: "#7BAE7F",
@@ -16,6 +18,7 @@ const drinks = [
   {
     image: "/images/orange.png",
     can: "/images/orangeCan.png",
+    bottle: "/images/orangeBottle.png",
     name: "orange",
     taste: "Orange",
     color: "#C75B12",
@@ -23,6 +26,7 @@ const drinks = [
   {
     image: "/images/cricket.png",
     can: "/images/cricketCan.png",
+    bottle: "/images/cricketBottle.png",
     name: "cricket",
     taste: "Lemony",
     color: "#66A85E",
@@ -30,6 +34,7 @@ const drinks = [
   {
     image: "/images/lemon.png",
     can: "/images/lemonCan.png",
+    bottle: "/images/lemonBottle.png",
     name: "lemon",
     taste: "Lemon",
     color: "#D6B647",
@@ -37,6 +42,7 @@ const drinks = [
   {
     image: "/images/powerup.png",
     can: "/images/powerupCan.png",
+    bottle: "/images/powerupBottle.png",
     name: "powerup",
     taste: "Strong Cola",
     color: "#2E4A7D",
