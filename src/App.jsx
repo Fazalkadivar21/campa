045f6b43f69f83c5513e81dbbb3d574e78c04346
fazalkadivar21 <fazalkadivar21@gmail.com>
2025-07-22 22:18:05 +0530

    ADD: routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CursorProvider } from "./context/CursorContext";
 
 import MainLayout from "./layouts/MainLayout";
+import AuthLayout from "./layouts/AuthLayout";
 
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import Login from "./pages/Login";
+import SignUp from "./pages/Signup";
 
 function App() {
   return (
@@ -14,6 +17,10 @@ function App() {
           <Route element={<MainLayout />}>
             <Route path="/" element={<Home />} />
             <Route path="/products" element={<Products />} />
+          </Route>
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<SignUp />} />
           </Route>
           {/* you could also add a fallback 404 page here */}
         </Routes>
