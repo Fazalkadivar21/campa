4b36f55e1bd9cc29a246c3a50df42a7a997e2aee
fazalkadivar21 <fazalkadivar21@gmail.com>
2025-07-24 21:41:49 +0530

    ADD: loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CursorProvider } from "./context/CursorContext";
 
 import MainLayout from "./layouts/MainLayout";
@@ -9,27 +9,31 @@ import Products from "./pages/Products";
 import Login from "./pages/Login";
 import SignUp from "./pages/Signup";
 import ProductDetails from "./pages/ProductDetails";
-import ScrollToTop from './components/ScrollToTop';
+import ScrollToTop from "./components/ScrollToTop";
 import About from "./pages/About";
+import Loading from "./components/Loading";
+import { Suspense } from "react";
 
 function App() {
   return (
     <CursorProvider>
       <BrowserRouter>
         <ScrollToTop />
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/products/:name" element={<ProductDetails />} />
-            <Route path="/about" element={<About />} />
-          </Route>
-          <Route element={<AuthLayout />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<SignUp />} />
-          </Route>
-          {/* you could also add a fallback 404 page here */}
-        </Routes>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/products/:name" element={<ProductDetails />} />
+              <Route path="/about" element={<About />} />
+            </Route>
+            <Route element={<AuthLayout />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<SignUp />} />
+            </Route>
+            {/* you could also add a fallback 404 page here */}
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </CursorProvider>
   );
---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline";
+
+export default function App() {
+  return (
+    <div className="h-screen p-0 m-0">
+      <Spline scene="https://prod.spline.design/KCsFIM5Au0Cizmsd/scene.splinecode" />
+    </div>
+  );
+}
